0dc3d87d53b5a3574161282c5e0614e1bde3f71f
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-03-30 20:05:44 +0100

    d

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -25,7 +25,6 @@ export default function SignIn() {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData),
-        credentials: 'include'
       })
       const data = await res.json()
       if(data.success === false){
